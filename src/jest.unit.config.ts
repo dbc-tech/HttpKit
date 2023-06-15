57d05ffc13bef7643ebf098bc44f55c889582aaa
Neil Dobson <neil.dobson71@gmail.com>
2023-06-15 11:14:28 +1000

    Remove packages. Configure jest

--- src/jest.unit.config.ts
+++
@@ -1,7 +0,0 @@
-export default {
-  testEnvironment: 'node',
-  // eslint-disable-next-line prettier/prettier, no-useless-escape
-  testRegex: '.*\.test.?.ts$',
-  preset: 'ts-jest',
-  setupFilesAfterEnv: ['./setupTests.ts'],
-};

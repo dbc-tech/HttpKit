97adadf5dd6eb7ccc9dee79c6659752fd3386802
Neil Dobson <neil.dobson71@gmail.com>
2023-06-16 16:39:25 +1000

    Revert type change

--- src/utils/plain-to-dto.ts
+++ src/utils/plain-to-dto.ts
@@ -5,7 +5,8 @@ export type Unpack<T> = T extends Array<infer U> ? U : T;
 export type DtoConstructor<T> = new (...args: unknown[]) => Unpack<T>;
 
 export const plainToDto = <T>(
-  plain: unknown,
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  plain: any,
   dtoConstructor?: DtoConstructor<T>,
 ) => {
   const canTransform = dtoConstructor != null && plain != null;
